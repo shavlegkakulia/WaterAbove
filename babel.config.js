fdfd072ad49d9d2c8c85a0c53ae40467ca031b43
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2025-10-30 13:54:29 +0400

    Alias names added

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@api': './src/api',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
